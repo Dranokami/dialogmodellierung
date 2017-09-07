dcef580888236ad852e58ece306925173016b44d
khaake <kristin.haake@uni-bielefeld.de>
2017-09-07 12:45:59 +0200

    added new function stuctur

---
+++ v0.1/web_scraper/funktionsaufbau.py
@@ -0,0 +1,35 @@
+## web scraper function structure
+
+def pipeline(param, param, param):
+    ''' commit json '''
+    
+    return json_file
+
+
+def check_json():
+    ''' checks if date-json can be found in folder'''
+    
+    
+    return boolean
+    
+
+def call_page(boolean):
+    ''' calls mensa page '''
+       
+    if could_be_parsed: 
+        return mensa_doc
+    else:
+        return 'Seite konnte nicht aufgerufen werden'
+
+        
+def parsing(doc):
+    '''parses mensa structure'''
+    
+    if structure_correct:
+        return json_file
+    else:
+        return 'Die Struktur stimmt wieder'
+        
+def output(json_file):
+    ''' saves the json_file in folder'''
+    
